0cd2819dbae1847744bbb1838d87fa66ecca994d
DashaDafusun <84176953+DashaDafusun@users.noreply.github.com>
2021-10-30 14:41:35 +0200

    Add files via upload

---
+++ opdracht_24_eventHandlers/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+
+let button = document.querySelector("#mybutton");
+
+button.addEventListener('click', function () {
+    alert("button clicked");
+
+});
+
+let secondButton = document.querySelector("#secondButton");
+
+// secondButton.addEventListener('click', function changeColor() {
+//     document.body.classList.add('red-background');
+// });
+
+
+secondButton.addEventListener('click', function toggleColor() {
+    document.body.classList.toggle('red-background');
+
+})
